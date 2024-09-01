4b50abb2a207f5807cc77d44971ecd6ceaaf6404
Rxfa <rafa3rnrn@gmail.com>
2024-09-01 16:56:16 +0100

    Solved 36

---
+++ 36_Valid_Sudoku.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for row_idx, row in enumerate(board):
+            for col_idx, cell in enumerate(row):
+                if cell.isdigit():
+                    if cell in rows[row_idx]:
+                        return False
+                    if cell in cols[col_idx]:
+                        return False
+                    box_idx = (row_idx // 3) * 3 + (col_idx // 3)
+                    if cell in boxes[box_idx]:
+                        return False
+
+                    rows[row_idx].add(cell)
+                    cols[col_idx].add(cell)
+                    boxes[box_idx].add(cell)
+        return True
